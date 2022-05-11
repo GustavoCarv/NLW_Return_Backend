4a79191fd31a37e588e9cd439aac1e27754c338e
GustavoCarv <gustavo.carvalho@econverse.com.br>
2022-05-10 22:50:41 -0300

    feat: prisma linked and nodemail added

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,45 @@
 import express from "express";
+import nodemailer from "nodemailer";
+import { prisma } from "./prisma";
 
 const app = express();
 
+app.use(express.json());
 
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "e839dd7bb6fc32",
+    pass: "84c19965e6ea51",
+  },
+});
 
-app.listen(3333, () => {
-  console.log("Server started on port 3333");
+app.post("/feedbacks", async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    },
+  });
+
+  await transport.sendMail({
+    from: ' "Feedback" <opa@feedget.com>',
+    to: "Gus <gustavo.carvg@gmail.com>",
+    subject: "Novo feedback",
+    html: [
+      `<h1>Novo feedback</h1>`,
+      `<p>${type}</p>`,
+      `<p>${comment}</p>`,
+    ].join("\n"),
+  });
+
+  return res.status(201).json({ data: feedback });
+});
+
+app.listen(process.env.PORT || 3333, () => {
+  console.log("HTTP server running!");
 });

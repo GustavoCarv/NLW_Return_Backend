88cbd9b0142ae0ca4f2cc2ec4cdcc9c2a25825f0
GustavoCarv <gustavo.carvalho@econverse.com.br>
2022-05-16 23:05:52 -0300

    refactor: alteração e organização de pastas

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import nodemailer from "nodemailer";
 import { prisma } from "./prisma";
+import { PrismaFeedbacksRepository } from "./repositories/prisma/prisma-feedback-repository";
+import { SubmitFeedbackUseCase } from "./use-cases/submit-feedback-use-case";
 
 export const routes = express.Router();
 
@@ -16,14 +18,9 @@ const transport = nodemailer.createTransport({
 routes.post("/feedbacks", async (req, res) => {
   const { type, comment, screenshot } = req.body;
 
-  const feedback = await prisma.feedback.create({
-    data: {
-      type,
-      comment,
-      screenshot,
-    },
-  });
-
+  const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
+  const submitFeedbackUseCase = new SubmitFeedbackUseCase(prismaFeedbacksRepository);
+  /* 
   await transport.sendMail({
     from: ' "Feedback" <opa@feedget.com>',
     to: "Gus <gustavo.carvg@gmail.com>",
@@ -33,7 +30,7 @@ routes.post("/feedbacks", async (req, res) => {
       `<p>${type}</p>`,
       `<p>${comment}</p>`,
     ].join("\n"),
-  });
+  }); */
 
   return res.status(201).json({ data: feedback });
 });

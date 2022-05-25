d2837528e24bb69e6612e63ff3036f3964f87cf4
GustavoCarv <gustavo.carvalho@econverse.com.br>
2022-05-24 23:36:21 -0300

    refactor: envio de email agora é feito no espaço correto

--- src/routes.ts
+++ src/routes.ts
@@ -6,15 +6,16 @@ import { SubmitFeedbackUseCase } from "./use-cases/submit-feedback-use-case";
 
 export const routes = express.Router();
 
-
-
 routes.post("/feedbacks", async (req, res) => {
   const { type, comment, screenshot } = req.body;
 
   const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
   const nodemailerMailAdapter = new NodemailerMailAdapter();
 
-  const submitFeedbackUseCase = new SubmitFeedbackUseCase(prismaFeedbacksRepository, nodemailerMailAdapter);
+  const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+    prismaFeedbacksRepository,
+    nodemailerMailAdapter
+  ); 
 
   await submitFeedbackUseCase.execute({ type, comment, screenshot });
 
